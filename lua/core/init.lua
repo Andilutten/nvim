d111a477bc80ec04e3e1ec69d154f8d912236078
Andreas Malmqvist <andreasmalmqvist1992@gmail.com>
2021-04-08 14:34:12 +0200

    autocommands theiving

--- lua/core/init.lua
+++ lua/core/init.lua
@@ -1,14 +1,13 @@
+local env = vim.env
+
+env.EDITOR = 'nvr -cc split --remote-wait'
+
 require'core.options'
 require'core.packages'
 require'core.keymaps'
 require'core.lsp'
 require'core.ui'
-
--- TODO Move to own module
-vim.env.EDITOR = 'nvr -cc split --remote-wait'
-
--- TODO Fix this nasty stuff
-vim.cmd[[autocmd FileType gitcommit,gitrebase,gitconfig set bufhidden=delete]]
+require'core.autocmds'
 
 -- TODO Move to own module
 require'nvim-treesitter.configs'.setup {
